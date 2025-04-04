0e4a491776f117fdba51d37d80983cc99cc3cb97
Rogerio Paulo <rogeriopaulos@gmail.com>
2025-04-04 16:47:02 -0300

    refactor: Melhora a estrutura da página de login e adiciona ícones e logos

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,15 +1,16 @@
 'use client'
 
-import * as React from 'react';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import { Context } from '../../context';
 import axios from 'axios';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import * as React from 'react';
+import { Context } from '../../context';
 
 const LoginPage = () => {
 
@@ -50,53 +51,82 @@ const LoginPage = () => {
   return (
     <Box
       sx={{
-        marginTop: 10,
-        marginInline: 20,
+        height: '100vh',
+        width: '100vw',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
+        overflow: 'hidden',
       }}
     >
-      <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        Login
-      </Typography>
-      <Box component="form" noValidate sx={{ mt: 1 }}>
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        <Button
-          onClick={() => onLoginPressed()}
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Login
-        </Button>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          p: 4,
+          maxWidth: 400,
+          width: '100%',
+          boxShadow: 3,
+          borderRadius: 2,
+          bgcolor: 'background.paper',
+        }}
+      >
+        <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+            <AccountBalanceIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5" color={'primary.main'}>
+            Consulta <strong>Pix</strong>
+          </Typography>
+        </Box>
+        <Box component="form" noValidate sx={{ mt: 1, width: '100%' }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Senha"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <Button
+            onClick={() => onLoginPressed()}
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Entrar
+          </Button>
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+          <Image
+            src={'/logo_Lab_sm.png'}
+            alt='Logo_Lab_sm'
+            style={{ alignSelf:'center', margin:'10px' }}
+            height={20}
+            width={20} />
+          <Typography component="p" sx={{ color: 'text.secondary', fontWeight: 300, fontSize: 12 }}>
+            PCPI/DIPC/LAB-LD
+          </Typography>
+        </Box>
       </Box>
     </Box>
   )
